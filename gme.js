d873aa1701e9c3cdc982c42c06d9325d280267f3
Wolf Bergenheim <wolf@bergenheim.net>
2014-02-02 14:36:49 -0500

    Directions

--- gme.js
+++ gme.js
@@ -1,5 +1,9 @@
 var map;
 var infowindow;
+var geocoder;
+var directionsDisplay;
+var directionsService;
+var alternate = false;
 
 function initialize() {
     var mapOptions = {
@@ -14,6 +18,12 @@ function initialize() {
     };
     map = new google.maps.Map(document.getElementById('map-canvas'),
                               mapOptions);
+
+    geocoder = new google.maps.Geocoder();
+    directionsService = new google.maps.DirectionsService();
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    directionsDisplay.setMap(map);
+
     infowindow = new google.maps.InfoWindow({
         content: '<div style="width:15em;">&nbsp;</div>'
     });
@@ -26,14 +36,90 @@ function initialize() {
     });
     google.maps.event.addListener(mapsEngineLayer, 'click', function(event) {
         infowindow.close();
-        infowindow.setContent(event.infoWindowHtml);
+        var content = '<div class="googft-info-window">' + event.infoWindowHtml +
+          '<div class="googft-info-window"><a href="#" onclick="getdirections('+
+          event.featureId+','+event.latLng.lat()+','+event.latLng.lng()+');">opastus<a></div></div>';
+        infowindow.setContent(content);
         infowindow.setPosition(event.latLng);
-        anchor = new google.maps.MVCObject();
+        var anchor = new google.maps.MVCObject();
         anchor.set('position', event.latLng);
 //        anchor.set('anchorPoint', event.pixelOffset);
         infowindow.open(map, anchor);
     });
     
 }
+
+function getdirections(featureId, lat, lng) {
+    var latLng = new google.maps.LatLng(lat,lng);
+    // Create a div to hold the control.
+    infowindow.close();
+    var directionsDiv = document.getElementById('directions-widget');
+    document.getElementById('to').value = lat+","+lng;
+    document.getElementById('real-to').value = lat+","+lng;
+    if (map.controls[google.maps.ControlPosition.TOP_CENTER].length > 0) {
+        map.controls[google.maps.ControlPosition.TOP_CENTER].pop();
+    }
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(directionsDiv);
+    geocoder.geocode({'latLng': latLng}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            if (results[0]) {
+                document.getElementById('to').value = results[0].formatted_address;
+            }
+        }
+        directionsDiv.className = 'visible';
+    });
+}
+
+function doDirections(travelMode) {
+    var start = document.getElementById("from").value;
+    if (alternate) {
+        var end = document.getElementById("to").value;
+    } else {
+        var end = document.getElementById("real-to").value;
+    }
+    var request = {
+        origin:start,
+        destination:end,
+        travelMode: travelMode
+    };
+    console.log('Getting directions from ' + start + ' to ' + end);
+    directionsService.route(request, function(result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            console.log('FOund a route!');
+            directionsDisplay.setDirections(result);
+            directionsDisplay.setMap(map);
+            alternate = false;
+        }
+        else {
+            if (!alternate) {
+                alert("No direct route found.\nTrying to get as close as possible.");
+                alternate = true;
+                doDirections(travelMode);
+            } else {
+                alert("Route not found.\nCannot find a route. Please try another transportation method.");
+                alternate = false;
+            }
+        }
+    });
+}
+
+function closeDirections() {
+    map.controls[google.maps.ControlPosition.TOP_CENTER].pop();
+    directionsDisplay.setMap(null);
+}
+
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var geolocation = new google.maps.LatLng(
+          position.coords.latitude, position.coords.longitude);
+      autocomplete.setBounds(new google.maps.LatLngBounds(geolocation,
+          geolocation));
+    });
+  }
+}
+
 
 google.maps.event.addDomListener(window, 'load', initialize);
